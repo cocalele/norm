e899b966d9a6e81ed0c8d16711b27bd8210a987a
LiuLele <cocalele@gmail.com>
2020-01-23 21:42:05 +0800

    Make it compatible to assign value from int-like(BigInteger) to long

--- src/main/java/com/dieselpoint/norm/Query.java
+++ src/main/java/com/dieselpoint/norm/Query.java
@@ -1,5 +1,7 @@
 package com.dieselpoint.norm;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -207,8 +209,12 @@ public class Query {
 					for (int i = 1; i <= colCount; i++) {
 						String colName = meta.getColumnLabel(i);
 						Object colValue = sqlMaker.convertValue(rs.getObject(i), meta.getColumnTypeName(i));
-
-						pojoInfo.putValue(row, colName, colValue, true);
+						if(colValue instanceof BigDecimal)
+							pojoInfo.putValue(row, colName, ((BigDecimal)colValue).longValue(), true);
+						else if(colValue instanceof BigInteger)
+							pojoInfo.putValue(row, colName, ((BigInteger)colValue).longValue(), true);
+						else
+							pojoInfo.putValue(row, colName, colValue, true);
 					}
 					out.add(row);
 				}
